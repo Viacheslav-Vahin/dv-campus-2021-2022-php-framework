e4b136ff6caf4057d6db26a323b19a20666c1476
Viacheslav <vahin.vvv@gmail.com>
2021-12-16 10:18:10 +0200

    Lesson 9 - Fix code style, removed unnecessary code and style.

--- src/page.php
+++ src/page.php
@@ -5,6 +5,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
     <title>{DV.Campus} PHP Framework</title>
     <link rel="stylesheet" href="/css/main.min.css"/>
 </head>
